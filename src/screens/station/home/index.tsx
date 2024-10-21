1b3198b7823922c5e7eb1253b48226ffc8cf3f8a
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-21 16:36:21 -0300

    create station dashboard

---
+++ src/screens/station/home/index.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../../../components/navbar"
+
+export function StationDasboard() {
+    return (
+        <>
+        <main style={{display:"flex"}}>
+            <NavBar type="station" />
+            <h1>DASHBOARD DA ESTAÇÃO</h1>
+        </main>
+        </>
+    )
+}
