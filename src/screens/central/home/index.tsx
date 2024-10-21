d642b21ab5a3e09ed31bba56c6fed876751e8958
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-21 18:48:40 -0300

    add dashboard Central screen

--- src/screens/central/home/index.tsx
+++ src/screens/central/home/index.tsx
@@ -1,11 +1,448 @@
+import './centralDash.css'
 import { NavBar } from "../../../components/navbar"
 
 export function CentralDasboard() {
+    const unity = [
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+        {
+            id: '1',
+            name: 'unidade 15',
+            status: 'ACTIVE'
+        },
+    ]
+    const operators = [
+        {
+            id: '1',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '2',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '3',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '4',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '5',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '6',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '1',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '2',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '3',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '4',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '5',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '6',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '1',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '2',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '3',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '4',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '5',
+            station: '17236',
+            name: 'Luz'
+        },
+        {
+            id: '6',
+            station: '17236',
+            name: 'Luz'
+        },
+    ]
+    const register = [
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+        {
+            hour: '13:00',
+            unity: 'unidade 15',
+            leitor: '8',
+            status: 'CONFIRMADO'
+        },
+    ]
+
     return (
         <>
         <main style={{display:"flex"}}>
             <NavBar type="central" />
-            <h1>DASHBOARD DA CENTRAL</h1>
+            <section className="containerCentralDashboard">
+                <h1>Visão Geral</h1>
+                <div style={{height: '90%', display: 'flex', flexDirection: 'column', marginTop: 24}}>
+                    <div style={{height: '50%'}}>
+                        <label>Registros</label>
+                        <div className='table'>
+                            <header style={{display: 'flex'}}>
+                                <div style={{width: '25%'}}>Horário</div>
+                                <div style={{width: '25%'}}>Unidade</div>
+                                <div style={{width: '25%'}}>leitor</div>
+                                <div style={{width: '25%'}}>Status</div>
+                            </header>
+                            <div className='scrollable' style={{height:'90%', overflow:'auto'}}>
+                                {register.map((register) => (
+                                    <div style={{display: 'flex'}}>
+                                        <div style={{width: '25%'}}>{register.hour}</div>
+                                        <div style={{width: '25%'}}>{register.unity}</div>
+                                        <div style={{width: '25%'}}>{register.leitor}</div>
+                                        <div style={{width: '25%'}}>{register.status}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    {/* DIVIDES */}
+                    <div style={{height: '50%', display: 'flex', gap: 12}}>
+                        <div style={{width: '50%'}}>
+                            <label>Unidades</label>
+                            <div className='table'>
+                                <header style={{display: 'flex'}}>
+                                    <div style={{width: '33.3%'}}>ID</div>
+                                    <div style={{width: '33.3%'}}>Nome</div>
+                                    <div style={{width: '33.3%'}}>Status</div>
+                                </header>
+                                <div style={{height:'90%', overflow:'auto'}}>
+                                    {unity.map((unity) => (
+                                        <div style={{display: 'flex'}}>
+                                            <div style={{width: '33.3%'}}>{unity.id}</div>
+                                            <div style={{width: '33.3%'}}>{unity.name}</div>
+                                            <div style={{width: '33.3%'}}>{unity.status}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{width: '50%'}}>
+                            <label>Operadores</label>
+                            <div className='table'>
+                                <header style={{display: 'flex'}}>
+                                    <div style={{width: '33.3%'}}>ID</div>
+                                    <div style={{width: '33.3%'}}>Estação</div>
+                                    <div style={{width: '33.3%'}}>Nome</div>
+                                </header>
+                                <div style={{height:'90%', overflow:'auto'}}>
+                                    {operators.map((operator) => (
+                                        <div style={{display: 'flex'}}>
+                                            <div style={{width: '33.3%'}}>{operator.id}</div>
+                                            <div style={{width: '33.3%'}}>{operator.station}</div>
+                                            <div style={{width: '33.3%'}}>{operator.name}</div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
         </>
     )
