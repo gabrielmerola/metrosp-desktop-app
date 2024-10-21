1380d89787c51769a72204e9f9c4d6fbe2f3be05
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-21 19:00:55 -0300

    add station dashboard

--- src/screens/station/home/index.tsx
+++ src/screens/station/home/index.tsx
@@ -5,7 +5,35 @@ export function StationDasboard() {
         <>
         <main style={{display:"flex"}}>
             <NavBar type="station" />
-            <h1>DASHBOARD DA ESTAÇÃO</h1>
+            <section className="containerCentralDashboard">
+                <h1>Visão Geral</h1>
+                <div style={{height: '90%', display: 'flex', flexDirection: 'column', marginTop: 24}}>
+                    <div style={{height: '50%'}}>
+                        <label>Registros</label>
+                        <div className='table'>
+                            <header style={{display: 'flex'}}>
+                                <div style={{textAlign: 'center', width: '33.3%'}}>Horário</div>
+                                <div style={{textAlign: 'center', width: '33.3%'}}>leitor</div>
+                                <div style={{textAlign: 'center', width: '33.3%'}}>status</div>
+                            </header>
+                            <div className='scrollable' style={{height:'90%', overflow:'auto'}}>
+                                    <div style={{display: 'flex'}}>
+                                        <div style={{width: '33.3%'}}>1</div>
+                                        <div style={{width: '33.3%'}}>2</div>
+                                        <div style={{width: '33.3%'}}>3</div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* DIVIDES */}
+                    <div style={{height: '50%'}}>
+                        <label>Cameras</label>
+                        <div className='table'>
+                            {/* CAMERA */}
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
         </>
     )
