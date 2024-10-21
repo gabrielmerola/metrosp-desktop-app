f6c888ebeedfb57635e9c1090e378a985e0dc1d1
Gabriel Merola <gabrielmerola12@gmail.com>
2024-10-21 14:09:58 -0300

    Refactor Login component to add styling and update layout

--- src/screens/auth/Login/index.tsx
+++ src/screens/auth/Login/index.tsx
@@ -1,10 +1,20 @@
+import "./login.css";
 import { Link } from "react-router-dom";
+import logoGov from '../../../assets/governoSP.png'
 
 export function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to={'/'}>home</Link>
-        </div>
+        <main className="backgroundLogin">
+            <section className="backgroundBlue">
+                <div className="left">
+                    <img src={logoGov} alt="Logo Governo do Estado São Paulo" />
+                    <h1>ssss</h1>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <Link to={'/'}>home</Link>
+                </div>
+            </section>
+        </main>
     )
 }
