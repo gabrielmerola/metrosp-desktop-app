73dea5bcb0583447cb48190753f9df24b79bac0e
Gabriel Merola <gabrielmerola12@gmail.com>
2024-10-21 10:37:54 -0300

    Add Loading component with redirect to login page after 3 seconds

---
+++ src/Loading.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import logoGov from './assets/governoSP.png'
+import { useNavigate } from 'react-router-dom';
+
+export function Loading() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 3000);
+  }, [])
+  
+  return (
+    <div className='background-linear'>
+      <img src={logoGov} alt="Governo do Estado São Paulo" />
+      {/* <i style={{fontSize: '24px', color: 'white'}} className="fa-solid fa-spinner animation-spin"></i> */}
+    </div>
+  )
+}
