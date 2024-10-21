3019337286c68e0e943eed224fa67b9e83f394df
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-21 16:36:12 -0300

    create central dashboard

---
+++ src/screens/central/home/index.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "../../../components/navbar"
+
+export function CentralDasboard() {
+    return (
+        <>
+        <main style={{display:"flex"}}>
+            <NavBar type="central" />
+            <h1>DASHBOARD DA CENTRAL</h1>
+        </main>
+        </>
+    )
+}
